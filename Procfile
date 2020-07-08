cea9412406285ee0dd2a09ab109f24915838a61d
Keith Brown <keb686@g.harvard.edu>
2020-07-07 23:07:39 -0400

    Added settings

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar build/libs/curriculum-crawler-backend-0.0.1-SNAPSHOT.jar
--- src/main/java/edu/harvard/cscis71/curriculum/HomeController.java
+++ src/main/java/edu/harvard/cscis71/curriculum/HomeController.java
@@ -1,5 +1,7 @@
 package edu.harvard.cscis71.curriculum;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,11 +11,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class HomeController {
+    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
     public static final String REDIRECT_STRING = "swagger-ui.html";
 
     @RequestMapping("/")
     public String index() {
+        logger.info("Received request for index, redirecting...");
         return "redirect:" + REDIRECT_STRING;
     }
 
